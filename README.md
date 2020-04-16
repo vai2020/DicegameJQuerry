80b8f6658cfcac0309dcb3035cfc42e6e590a50f
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-15 21:39:06 -0500

    made copies of README and pseudocode files from original dice game in JS to JQuerry game

---
+++ README.md
@@ -0,0 +1,18 @@
+Dice-game (JQuerry version)
+
+A two-player game between Computer and User
+Each player will take turn to roll the dice
+A win state
+A lose state
+The game can continue
+Codes will be written in JS and JQuery with HTML and CSS using the DOM
+Multiple commits spread out on different days
+Will have a pseudocode file to showcase references and credits from outside sources relating to the game
+Will have a score card for the game
+Link to live site on github: https://github.com/vai2020/The_dice_game
+
+Game rules: Each dice has numbers from 1 to 6. When Computer's randomly generated number is higher than User's, then Computer wins. If User's randomly generated number is higher than Computer's, then User wins. If bothh parties's randomly generated numbers are the same, it's a tie game.
+
+Technology may include: If/then else statements; Math.floor(Math.random)functions. HTML, CSS. Game icons. 6 images/icons for the game
+
+Need to find 6 free dice icons from websites online. Here's the link to a potential free icon website: https://icons8.com/license
---
+++ pseudocode.js
@@ -0,0 +1,19 @@
+/* game icons of dice1, dice2, dice3, dice4, dice5 and dice6 come from https://icons8.com/license
+ 
+Inspirations came from the following sites and videos:
+https://www.bing.com/videos/search?q=how+to+write+an+easy+js+game&docid=607990489090557689&mid=EEE8568F2BA345BF593EEEE8568F2BA345BF593E&view=detail&FORM=VIRE
+ 
+ 
+ 
+Other related references:
+ 
+https://www.w3schools.com/js/js_htmldom.asp
+https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps
+https://www.keycdn.com/blog/javascript-performance
+https://www.w3schools.com/jquery/jquery_callback.asp
+https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_html_text_set //jquery .text or text()method
+https://teamtreehouse.com/community/why-do-we-put-the-jquery-script-in-the-beginning-of-the-html-instead-of-at-the-end-near-the-body
+https://forums.asp.net/t/2106876.aspx?jquery+works+when+put+just+before+body+tag+but+does+not+work+in+head+head+section+  //the posiition of jquery affecting performance
+jQuery text() method: https://www.w3schools.com/jquery/jquery_dom_get.asp
+jQuery selector: https://www.w3schools.com/jquery/jquery_selectors.asp
+jQuery .click() method: https://www.w3schools.com/jquery/event_click.asp
