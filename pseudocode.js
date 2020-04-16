53b7a7a112a364dd9ba62f681d48136e31e3b5b1
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-15 23:59:34 -0500

    style E

--- pseudocode.js
+++ pseudocode.js
@@ -17,3 +17,6 @@ https://forums.asp.net/t/2106876.aspx?jquery+works+when+put+just+before+body+tag
 jQuery text() method: https://www.w3schools.com/jquery/jquery_dom_get.asp
 jQuery selector: https://www.w3schools.com/jquery/jquery_selectors.asp
 jQuery .click() method: https://www.w3schools.com/jquery/event_click.asp */
+
+
+            /*$("#Final").text("Tie game " + "Player score: " + player + " vs " + "Computer Score: " + computer); */
